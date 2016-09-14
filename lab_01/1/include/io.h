40fb20c90075159a80febcc10fc5ba0b16b5069d
oquechy <vasilliza@gmail.com>
2016-09-14 11:05:56 +0300

    lab1

--- lab_01/1/include/io.h
+++ lab_01/1/include/io.h
@@ -1,4 +1,4 @@
 #ifndef IO
-#define IO
+#define IO             
 void logDebug(char c);
 #endif
--- lab_01/1/src/main.c
+++ lab_01/1/src/main.c
@@ -1,7 +1,7 @@
 #include "algorithm.h"
 #include "util.h"
 
-int main() {
+int main() {     
   int a = doComputation(5, 7.0); 
   int b = 5;
   max(a, b);
